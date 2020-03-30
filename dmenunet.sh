72583892e2f1d1762c4fce06560931b05e11496a
M4t35Z <M4t35Z@protonmail.com>
2020-03-30 12:50:42 +0200

    manage net with dmenu

--- dmenunet.sh
+++ dmenunet.sh
@@ -3,7 +3,8 @@
 # wpa_passphrase <SSID> <PASSWD> > /etc/wpa_supplicant/wpa_supplicant_<SSID>.conf
 # sudo wpa_supplicant -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant_<SSID>.conf
 
-case "$(printf "TP-LINK_BCF4\\ndhclient" | dmenu -i -p "Wifi settings")" in
+case "$(printf "TP-LINK_BCF4\\neth0\\ndhclient" | dmenu -i -p "Internet")" in
     "TP-LINK_BCF4") terminator -e "sudo wpa_supplicant -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant_TP-LINK_BCF4.conf" ;;
-    "dhclient") sudo dhclient ;;
+    "dhclient") sudo -A dhclient ;;
+    "eth0") sudo -A ip link set eth0 up ;;
 esac
