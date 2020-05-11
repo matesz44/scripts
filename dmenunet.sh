c73951a1956645713d2c4dfba5d0bc74239b152a
matesz44 <matesz44>
2020-05-11 09:43:29 +0200

    st didnt work :/

--- dmenunet.sh
+++ dmenunet.sh
@@ -4,7 +4,7 @@
 # sudo wpa_supplicant -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant_<SSID>.conf
 
 case "$(printf "TP-LINK_BCF4\\neth0\\ndhclient" | dmenu -i -p "Internet")" in
-    "TP-LINK_BCF4") $TERMINAL -e "sudo wpa_supplicant -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant_TP-LINK_BCF4.conf" ;;
+    "TP-LINK_BCF4") terminator -e "sudo wpa_supplicant -i wlan0 -c /etc/wpa_supplicant/wpa_supplicant_TP-LINK_BCF4.conf" ;;
     "dhclient") sudo -A dhclient ;;
     "eth0") sudo -A ip link set eth0 up ;;
 esac
